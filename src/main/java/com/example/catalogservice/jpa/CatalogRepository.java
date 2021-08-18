1f3276d98a0a71efd15a0ff838f57b27c5811d63
byeungoo <dksekfldks65@naver.com>
2021-08-18 23:45:56 +0900

    catalog service 카프카 적용

--- src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
+++ src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
 
 
-
+    CatalogEntity findByProductId(String productId);
 }
---
+++ src/main/java/com/example/catalogservice/messagequeue/KafkaConsumerConfig.java
@@ -0,0 +1,45 @@
+package com.example.catalogservice.messagequeue;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+
+        //카프카 서버 주소
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+        //카프카에서 토픽에 쌓여있는 메세지를 가져가는 컨슈머들을 그룹핑할 수 있음.
+        //지금은 컨슈머가 하나라 별 의미는 없긴함.
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
+
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+
+        return kafkaListenerContainerFactory;
+    }
+
+}
